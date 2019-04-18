9d70648adfec25a1e203f4b4ac5142ce0fd8c111
Ssparker <47154706+Ssparker@users.noreply.github.com>
2019-04-18 18:57:23 -0500

    fizzbuzz Lab

---
+++ src/Runner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Runner {
+
+        public static void main (String[] args) {
+            Scanner in = new Scanner(System.in);
+
+            System.out.println("Enter a user name:");
+            String userName = in.next();
+            System.out.println("Enter a number:");
+            Integer inputNumber = in.nextInt();
+
+
+        }
+
+//        inputs = Takeinputs
+//
+//        view = controller.handle(inputs);
+//
+//            service
+}
+
