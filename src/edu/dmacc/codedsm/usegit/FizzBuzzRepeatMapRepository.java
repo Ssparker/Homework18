bc5189362765ee5f0f4504186fd23ba610d7647a
Ssparker <47154706+Ssparker@users.noreply.github.com>
2019-04-22 17:27:02 -0500

    8th commit

--- src/edu/dmacc/codedsm/usegit/FizzBuzzRepeatMapRepository.java
+++ src/edu/dmacc/codedsm/usegit/FizzBuzzRepeatMapRepository.java
@@ -1,5 +1,9 @@
 package edu.dmacc.codedsm.usegit;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +20,18 @@ public class FizzBuzzRepeatMapRepository implements MapRepository {
     }
 
      public void save(Result result) {
+
+         File file = new File("fizzbuzz_result.txt");
+         PrintWriter pw = null;
+         try {
+             pw = new PrintWriter(file);
+             pw.println("result = "+result.toString());
+         } catch(FileNotFoundException e1) {
+             e1.printStackTrace();
+         } finally {
+             pw.close();
+         }
+
         storage.put(1, result);
 
     }
--- src/edu/dmacc/codedsm/usegit/Runner.java
+++ src/edu/dmacc/codedsm/usegit/Runner.java
@@ -17,44 +17,20 @@ public class Runner {
         System.out.println("Enter a number:");
         Integer inputNumber = in.nextInt();
 
-        // MapRepository repository = new FizzBuzzMapRepository(); // change for new imp  switch back  / swap out files
-        //FizzBuzzService service = newFizzBuzzServiceImpl(repository);
-        MapRepository repository = new FizzBuzzRepeatMapRepository();
-        FizzBuzzService service = new FizzBuzzRepeatServiceImpl(repository);  // change for new imp  swtich back
+        MapRepository repository = new FizzBuzzMapRepository(); // change for new imp  switch back  / swap out files
+        FizzBuzzService service = new FizzBuzzServiceImpl(repository);
+        MapRepository altrepository = new FizzBuzzRepeatMapRepository();
+        FizzBuzzService altservice = new FizzBuzzRepeatServiceImpl(altrepository);  // change for new imp  swtich back
         SubmissionControl controller = new SubmissionControlImpl(service);
+        SubmissionControl altcontroller = new SubmissionControlImpl(altservice);
 
         InputView view = controller.submit(inputNumber, userName);
         view.render();
 
+        view = altcontroller.submit(inputNumber, userName);
+        view.render();
 
-                File file = new File("C:/development/Homework18/fizzbuzz_result.txt");
-                        if(!file.exists());
 
-                        try
-
-                {
-                    file.createNewFile();
-                }catch(
-                IOException e1)
-
-                {
-                    e1.printStackTrace();
-                }
-
-                PrintWriter pw = null;
-                        try
-
-                {
-                    pw = new PrintWriter(file);
-                } catch(
-                FileNotFoundException e1)
-
-                {
-                    e1.printStackTrace();
-                }
-                        pw.println("result = "+repository.toString());
-
-                        pw.close();
                         System.out.println("DONE");
     }
     //code cotta - prescribed set of steps do over and over again
